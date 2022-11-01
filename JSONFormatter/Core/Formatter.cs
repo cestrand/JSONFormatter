06176df85d87fbbc5b116bb5f7d6aebe200ec10c
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-01 16:42:04 +0100

    Add project files.

---
+++ JSONFormatter/Core/Formatter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONFormatter.Core;
+public class Formatter
+{
+    public readonly Settings settings;
+
+    public Formatter(Settings settings)
+    {
+        this.settings = settings;
+    }
+
+    public Formatter() : this(new Settings()) { }
+
+    public Output Format(string s)
+    {
+        var result = s;
+        return new Output(result, null);
+    }
+
+    public record Settings(
+        ushort IndentSize = 2
+    );
+
+    public record Output(
+        string result,
+        string? error
+    )
+    {
+        public override string ToString() => result;
+        public static implicit operator string(Output o) => o.result;
+    }
+}
---
+++ JSONFormatterTests/Core/FormatterTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSONFormatter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONFormatter.Core.Tests;
+
+[TestClass()]
+public class FormatterTests
+{
+    readonly Formatter defaultFormatter = new();
+
+    [TestMethod()]
+    public void FormatTest__EmptyString()
+    {
+        Assert.AreEqual(
+            "",
+            defaultFormatter.Format(""));
+    }
+}
